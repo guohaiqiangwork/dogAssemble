6ef4a3514eb6ec0d83d82fd186a0023f584cf25c
guohaiqiang <837557495@qq.com>
2019-05-16 17:36:25 +0800

    修改密码页面

--- src/views/changePassword/index.vue
+++ src/views/changePassword/index.vue
@@ -1,32 +1,101 @@
 <template>
   <div>
-        我是修改密码
+    <div class="pass_list_w">
+      <div class="search_box">
+        <img src="../../assets/images/手机@2x.png" class="width_16">
+        <input
+          type="text"
+          placeholder="请输⼊您的手机号"
+          style="width:100%;height:100%;outline: none;border:none"
+        >
+        <i></i>
+      </div>
+    </div>
+    <div class="pass_list_w">
+      <div class="search_box">
+        <img src="../../assets/images/验证码@2x.png" class="width_16">
+        <input
+          type="text"
+          placeholder="请输⼊验证码"
+          style="width:64%;height:100%;outline: none;border:none"
+        >
+        <span class="margin_left_div3 font_color_4A" style="font-size:6px">获取验证码</span>
+      </div>
+    </div>
+    <div class="pass_list_w">
+      <div class="search_box">
+        <img src="../../assets/images/密码@2x.png" class="width_16">
+        <input
+          type="text"
+          placeholder="请设置您的新密码"
+          style="width:100%;height:100%;outline: none;border:none"
+        >
+        <i></i>
+      </div>
+    </div>
+    <!-- 确认 -->
+       <div class="text_center">
+      <div class="b_t_t">确认</div>
+    </div>
   </div>
 </template>
 <script>
 import url from "../../bin/url";
 export default {
-
   name: "changepassword",
   data() {
-    return {
-     
-    };
+    return {};
   },
-  methods: {
-
-  },
+  methods: {},
   created() {
     settitle("修改密码");
     this.routeParams = JSON.parse(this.$route.params.obj);
   },
 
   mounted() {
-   console.log('修改密码')
-
+    console.log("修改密码");
   }
 };
 </script>
 <style scoped>
-
+.pass_list_w {
+  width: 95%;
+  margin-left: 2%;
+}
+.search_box {
+  height: 3.5rem;
+  margin: 0.65rem 1.5rem 0;
+  position: relative;
+  padding: 0 1.55rem;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  z-index: 1;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  background: #fff;
+  line-height: 3.5rem;
+  margin-top: 5%;
+  border: none;
+  border-bottom: 1px solid rgb(173, 165, 165);
+}
+.width_16 {
+  width: 16px;
+  height: 16px;
+  margin-right: 3%
+}
+.b_t_t {
+  width: 96%;
+  background-color: #4a7b67;
+  color: #fff;
+  font-size: 16px;
+  line-height: 3;
+  border-radius: 5px;
+  margin-left: 2%;
+  position: fixed;
+  bottom: 5%;
+}
 </style>
