0bd6c5ab261ee6de967b11942ba517eaf2170f6e
sldcool <a451986097@163.com>
2019-07-02 15:39:07 +0800

    修改

--- src/bin/fetch.js
+++ src/bin/fetch.js
@@ -39,25 +39,17 @@ service
     /**
      * code为非200是错误的请求
      */
-    if (response.data.code == "702") {
-      alert(response.data.obj + '接口返回链接')
-      window.location.href = response.data.obj
-    }
-    if (response.data.msg == "openId_none!") {
-      alert(response.data.obj + '接口返回链接')
-      window.location.href = response.data.obj
-    }
-    // if (response.data.msg == "openId_error") {
+    // if (response.data.code == "702") {
     //   alert(response.data.obj + '接口返回链接')
     //   window.location.href = response.data.obj
     // }
-    // if (response.data.msg == "openId_error!") {
+    // if (response.data.msg == "openId_none!") {
     //   alert(response.data.obj + '接口返回链接')
     //   window.location.href = response.data.obj
     // }
-    if (response.data.msg == "no_login") {
-      router.push('/login/1');
-    }
+    // if (response.data.msg == "no_login") {
+    //   router.push('/login/1');
+    // }
     if (response.data.code == 300) {
       // alert(response.data.message)
       // Vue.$vux.toast.show({
