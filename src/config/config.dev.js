c01a748bd23286deae5b0ad599d78c4683bc6f4b
sldcool <a451986097@163.com>
2019-07-03 18:37:43 +0800

    修改

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,11 +1,9 @@
-
 //基础配置信息
 // var path = require('path')
 
 module.exports = {
     BACK_END:"/back",
     baseURL:"http://192.168.3.12:80",
-    //baseURL:"http://192.168.3.5:8085",
     // product:"http://904cdd07.ngrok.io"
     product:'http://www.gsb.yuntunet.cn'
   };
