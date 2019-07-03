ad7c7946df5f7ea7587da49a92851c9585a91d5d
guohaiqiang <837557495@qq.com>
2019-07-03 11:22:05 +0800

    处理推荐门店

--- src/views/recommend/index.vue
+++ src/views/recommend/index.vue
@@ -2,85 +2,57 @@
   <div>
     <!-- 第一部分 -->
     <div>
-      <div class="margin_top_div4 margin_left_div3">基本信息</div>
+      <div class="margin_top_div4 margin_left_div3 font_size_16 font_color_1A">基本信息</div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">联系人：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请输入姓名">
-        </div>
-      </div>
-      <div class="div_display_flex margin_left_div3 margin_top_div5">
-        <div class="div_width_25 font_size_14 font_color_1A">联系人：</div>
-        <div class="div_width_70">
-          <input type="text" placeholder="请输入姓名">
+          <input type="text" placeholder="请输入姓名" v-model="recommendList.linkman">
         </div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">手机号：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请输入手机号">
+          <input type="text" placeholder="请输入手机号" v-model="recommendList.linkPhone">
         </div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">商户名称：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请输入商户名称">
+          <input type="text" placeholder="请输入商户名称" v-model="recommendList.name">
         </div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">门店类型：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请输入门店类型">
-        </div>
-      </div>
-      <div class="div_display_flex margin_left_div3 margin_top_div5">
-        <div class="div_width_25 font_size_14 font_color_1A">营业时间：</div>
-        <div class="div_width_70">
-          <input type="text" placeholder="请选择开始营业时间 ">
-        </div>
-      </div>
-      <div class="div_display_flex margin_left_div3 margin_top_div5">
-        <div class="div_width_25 font_size_14 font_color_1A"></div>
-        <div class="div_width_70">
-          <input type="text" placeholder="请选择结束营业时间 ">
+          <input type="text" placeholder="请输入门店类型" v-model="recommendList.type">
         </div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">所属地区：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请选择店铺所在地区 ">
-        </div>
-      </div>
-      <div class="div_display_flex margin_left_div3 margin_top_div5">
-        <div class="div_width_25 font_size_14 font_color_1A">详细地址</div>
-        <div class="div_width_70"></div>
-      </div>
-      <div class="div_display_flex margin_left_div3 margin_top_div5">
-        <div class="div_width_25 font_size_14 font_color_1A"></div>
-        <div class="div_width_70">
-          <textarea name id cols="30" rows="10"></textarea>
+          <input type="text" placeholder="请选择店铺所在地区 " v-model="recommendList.aname1">
         </div>
       </div>
     </div>
     <!-- 第二部分 -->
     <div>
-      <div class="margin_top_div4 margin_left_div3">账户信息</div>
+      <div class="margin_top_div4 margin_left_div3 font_size_16 font_color_1A">账户信息</div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">姓名：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请输入姓名">
+          <input type="text" placeholder="请输入姓名" v-model="recommendList.accountName">
         </div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">银行名称：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请输入银行名称">
+          <input type="text" placeholder="请输入银行名称" v-model="recommendList.bankName">
         </div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
         <div class="div_width_25 font_size_14 font_color_1A">银行帐户：</div>
         <div class="div_width_70">
-          <input type="text" placeholder="请输入银行帐户">
+          <input type="text" placeholder="请输入银行帐户" v-model="recommendList.bankNum">
         </div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
@@ -88,62 +60,66 @@
         <div class="div_width_70"></div>
       </div>
       <div class="div_display_flex margin_left_div3 margin_top_div5">
-        <div class="div_width_25 font_size_14 font_color_1A"></div>
-        <div class="div_width_70">
-          <textarea name id cols="30" rows="10"></textarea>
-        </div>
+        <!-- <div class="div_width_25 font_size_14 font_color_1A"></div>
+        <div class="div_width_70">-->
+        <textarea name id rows="5" v-model="recommendList.bankAddress" style="width:95%"></textarea>
+        <!-- </div> -->
       </div>
+      <!-- 图片上传 -->
       <div>
-        <div class="div_width_25 font_size_14 font_color_1A">开户地址：</div>
-        <div class="div_width_70"></div>
-      </div>
-      <div class="div_display_flex margin_left_div3 margin_top_div5">
-        <div class="div_width_25 font_size_14 font_color_1A"></div>
-        <div class="div_width_70">
-          <textarea name id cols="30" rows="10"></textarea>
+        <div>
+          <div class="div_width_25 font_size_14 margin_left_div3 margin_top_div4 font_color_1A font_size_16 font_color_1A">资质信息</div>
+          <div class="div_width_70"></div>
         </div>
-      </div>
-      <div>
-        <div class="div_width_25 font_size_14 font_color_1A">资质信息:</div>
-        <div class="div_width_70"></div>
-      </div>
-      <div class="div_display_flex margin_left_div3 margin_top_div5 text_center">
-        <div class="up_div">
-          <img src="../../assets/images/Bluetooth.png" alt>
+        <div class="div_display_flex margin_left_div3 margin_top_div5 text_center">
+          <!-- <div class="up_div">
+            <img src="../../assets/images/Bluetooth.png" alt>
+          </div>-->
+          <div class="img_up_list">
+            <img src="../../assets/images/000@2x.png" width="20px" style="margin-top:25%" alt>
+          </div>
         </div>
-      </div>
-      <div>图片上传类型：jpg、png、gif ; 最多10张；文件大小为5M</div>
-      <input
-        @change="fileChange($event)"
-        type="file"
-        id="upload_file"
-        multiple
-        style="display: none"
-      >
-      <div class="add" @click="chooseType">
-        <div class="add-image" align="center">
-          <!--按钮中的图片是一个icon字体图标 -->
-          <i class="icon-camera"></i>
-          <p class="font13">添加图片</p>
+        <div class="div_display_flex margin_top_div4">
+          <div class="margin_left_div3">
+            <img src="../../assets/images/111@2x.png" width="12px" alt>
+          </div>
+          <div class="font_size_12" style="color:#242E42;margin-left:1%">图片上传类型：jpg、png、gif ; 最多10张；文件大小为5M</div>
         </div>
-      </div>
-      <div class="add-img" v-show="imgList.length">
-        <p class="font14">
-          图片(最多10张，还可上传
-          <span v-text="6-imgList.length"></span>张)
-        </p>
-        <ul class="img-list">
-          <li v-for="(url,index) in imgList" :key="index">
-            <!-- <img
+
+        <input
+          @change="fileChange($event)"
+          type="file"
+          id="upload_file"
+          multiple
+          style="display: none"
+        >
+        <div class="add" @click="chooseType">
+          <div class="add-image" align="center">
+            <!--按钮中的图片是一个icon字体图标 -->
+            <i class="icon-camera"></i>
+            <p class="font13">添加图片</p>
+          </div>
+        </div>
+        <div class="add-img" v-show="imgList.length">
+          <p class="font14">
+            图片(最多10张，还可上传
+            <span v-text="6-imgList.length"></span>张)
+          </p>
+          <ul class="img-list">
+            <li v-for="(url,index) in imgList" :key="index">
+              <!-- <img
               class="del"
               src="http://img.shtml.net/XGaC5nwBzmaeMPm0J-H12X-G3zjUOlYZmnX0J-H1wPM3gKpTqvYekJohZzYPGROtBz0J-H1wT4uAGgZV.jpg"
               @click.stop="delImg(index)"
-            > -->
-            <!-- //del删除样式，icon字体图标需要自己找哦 -->
-            <img :src="url.file.src"  @click.stop="delImg(index)">
-          </li>
-        </ul>
+              >-->
+              <!-- //del删除样式，icon字体图标需要自己找哦 -->
+              <img :src="url.file.src" @click.stop="delImg(index)">
+            </li>
+          </ul>
+        </div>
       </div>
+      <!-- 确认 -->
+      <div class="backgroun_color_4A font_color_ff font_size16 text_center" style="line-height:3" @click="getRegister">确认</div>
     </div>
   </div>
 </template>
@@ -157,7 +133,24 @@ export default {
       imgList: [],
       size: 0,
       limit: 10, //限制图片上传的数量
-      tempImgs: []
+      tempImgs: [],
+      recommendList: {
+        name: "", //商户名称
+        aname1:'',
+        type: "", //0：店铺果蔬吧，1：家庭果蔬吧）
+        linkman: "", //联系人
+        linkPhone: "", //手机号
+        province: "", //省
+        city: "", //市
+        area: "", //区
+        accountName: "", //账户名称
+        bankName: "", //银行名称
+        bankNum: "", //银行账户
+        bankAddress: "", //开户地址
+        openId: "",
+        picIds: [] //图片list
+      },
+      filedata:''
     };
   },
   methods: {
@@ -205,6 +198,7 @@ export default {
       });
     },
     fileAdd(file) {
+      let that = this
       if (this.limit !== undefined) this.limit--;
       if (this.limit !== undefined && this.limit < 0) return;
       //总大小
@@ -224,10 +218,13 @@ export default {
             let height = image.height;
             file.width = width;
             file.height = height;
+            that.filedata = file;//获取数据流
             _this.imgList.push({
               file
             });
             console.log(_this.imgList);
+            that.uplod()//上传
+            // that.recommendList.picIds = _this.imgList
           };
           image.src = file.src;
         };
@@ -238,7 +235,40 @@ export default {
       this.imgList.splice(index, 1);
       if (this.limit !== undefined) this.limit = 6 - this.imgList.length;
     },
-    displayImg() {}
+    displayImg() {},
+    // 图片上传
+    uplod(){
+          this.$fetch
+        .post("/fruits/app/blank/saveFile", {filedata:this.filedata})
+        .then(res => {
+          if (res.code == 0) {
+            console.log(res.obj);
+          } else {
+            alert(res.msg);
+          }
+        });
+    
+    },
+    // 保存推荐信息
+    getRegister() {
+      this.recommendList.openId = localStorage.getItem("openId");
+      this.$fetch
+        .post("/fruits/app/user/register", this.recommendList)
+        .then(res => {
+          if (res.code == 0) {
+            console.log(res.obj);
+            // res.obj.forEach(item => {
+            //   if (item.type == 0) {
+            //     this.huiYList.push(item);
+            //   } else {
+            //     this.biGList.push(item);
+            //   }
+            // });
+          } else {
+            alert(res.msg);
+          }
+        });
+    }
   },
   created() {
     settitle("我是推荐信息");
@@ -252,6 +282,13 @@ export default {
 </script>
 <style scoped>
 textarea {
-  border: 1px solid red;
+  border: 1px solid #f4f4f4;
+}
+.img_up_list {
+  background-color: #e9efec;
+  width: 136px;
+  height: 93px;
+  border: 1px solid #e9efec;
+  margin-left: 20%;
 }
 </style>
