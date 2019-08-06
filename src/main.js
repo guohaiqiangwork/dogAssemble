cc0be1107eba5239bf388f7a955493ac66fdf9d0
guohaiqiang <837557495@qq.com>
2019-08-06 14:55:09 +0800

    删除console.log

--- src/main.js
+++ src/main.js
@@ -35,7 +35,6 @@ window.settitle = settitle;
 
  router.beforeEach((to, from, next) => {
   // && to.name!="answer" && to.name!="result"&& to.name!="nearby" 答题和结果
-  console.log(to.name )
    if(!localStorage.getItem('user') && to.name !== "login" && to.name !== "shop" && to.name !=="home" && to.name!="changePassword" && to.name !=="introduce"&& to.name !=="sharedBonusTwo"&& to.name !=="nationalStores" && to.name !=="privacyProtocol"){
      next("/login/1");
      return 
