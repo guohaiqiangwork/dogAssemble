889c9948491a77942601249986c66beaf26cd7ec
guohaiqiang <837557495@qq.com>
2019-08-07 09:02:54 +0800

    地图导航

--- src/views/Tmap/index.vue
+++ src/views/Tmap/index.vue
@@ -6,6 +6,7 @@
 <script>
 import url from "../../bin/url";
 import { TMap } from "../../bin/TMap";
+import wx from "weixin-jsapi";
 export default {
   name: "Tmap",
   data() {
@@ -19,18 +20,82 @@ export default {
       latitude: "",
       longitude: "",
       latitudeZ: "",
-      longitudeZ: ""
+      longitudeZ: "",
+      latitudeQ: "", // 纬度，浮点数，范围为90 ~ -90
+      longitudeQ: ""
     };
   },
   methods: {
+    // 获取地理位置配置
+    getSharedBonus() {
+      this.$fetch
+        .post("/fruits/app/weChat/getWechatConfig", {
+          url: window.location.href.split("#")[0]
+        })
+        .then(
+          data => {
+            if (data.code == 0) {
+              this.wexinGetLog(data.obj);
+            } else {
+              alert(data.msg);
+            }
+          },
+          err => {
+            alert("网络缓慢。。");
+          }
+        );
+    },
+    // 微信获取位置
+    wexinGetLog(data) {
+      let that = this;
+      wx.config({
+        debug: false,
+        appId: data.appId,
+        nonceStr: data.noncestr,
+        timestamp: data.timestamp,
+        signature: data.sign,
+        jsApiList: ["checkJsApi", "openLocation", "getLocation"]
+      });
+      wx.checkJsApi({
+        jsApiList: ["getLocation"],
+        success: function(res) {
+          if (res.checkResult.getLocation == false) {
+            alert(
+              "你的微信版本太低，不支持微信JS接口，请升级到最新的微信版本！"
+            );
+            return;
+          }
+        }
+      });
+      wx.ready(function() {
+        wx.getLocation({
+          success: function(res) {
+            console.log("微信获取" + JSON.stringify(res));
+            that.longitudeQ = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+            that.latitudeQ = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+            console.log(that.latitudeQ + "发顺丰" + that.longitudeQ);
+            that.drawMap();
+          },
+          cancel: function(res) {
+            alert("用户拒绝授权获取地理位置");
+            // that.getShopFjStudio()
+          }
+        });
+      });
+      wx.error(function(res) {
+        //                        console.log(res)
+        // that.getShopFjStudio()
+      });
+    },
     //  获取当前位置
     drawMap() {
       // 专车--画地图
       let that = this;
+      console.log(that.latitudeQ + "fasa是的" + that.longitudeQ);
       var map = new AMap.Map("map_container", {
         resizeEnable: true,
         zoom: 14,
-        center: [that.latitude, that.longitude] // 地图中心点的经纬度
+        center: [that.latitudeQ, that.longitudeQ] // 地图中心点的经纬度
       });
       AMap.plugin("AMap.Driving", function() {
         var driving = new AMap.Driving({
@@ -38,13 +103,15 @@ export default {
           policy: AMap.DrivingPolicy.LEAST_TIME
         });
         //起、终点
-        var start_xy = new AMap.LngLat(that.longitude, that.latitude); // 起点的经纬度
+        var start_xy = new AMap.LngLat(that.latitudeQ, that.longitudeQ); // 起点的经纬度
+        console.log(start_xy + "jfkls");
         var end_xy = new AMap.LngLat(
-           Number(that.longitudeZ),
+          Number(that.longitudeZ),
           Number(that.latitudeZ)
         ); // 终点的经纬度
         // 根据起终点经纬度规划驾车导航路线
         driving.search(start_xy, end_xy, function(status, result) {
+          console.log(JSON.stringify(status));
           if (status === "complete") {
             if (result.routes && result.routes.length) {
               console.log(result.routes[0]);
@@ -89,31 +156,31 @@ export default {
         }
       }
       return path;
-    },
-    // 获取当前位置
-    getAdd() {
-      //获取地理位置
-      var self = this;
-      //全局的this在方法中不能使用，需要重新定义一下
-      var geolocation = new BMap.Geolocation();
-      //调用百度地图api 中的获取当前位置接口
-      geolocation.getCurrentPosition(function(r) {
-        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-          //获取当前位置经纬度
-          var myGeo = new BMap.Geocoder();
-          myGeo.getLocation(new BMap.Point(r.point.lng, r.point.lat), function(
-            result
-          ) {
-            if (result) {
-              console.log(result);
-              self.latitude = result.point.lat;
-              self.longitude = result.point.lng;
-              self.drawMap();
-            }
-          });
-        }
-      });
     }
+    // 获取当前位置
+    // getAdd() {
+    //   //获取地理位置
+    //   var self = this;
+    //   //全局的this在方法中不能使用，需要重新定义一下
+    //   var geolocation = new BMap.Geolocation();
+    //   //调用百度地图api 中的获取当前位置接口
+    //   geolocation.getCurrentPosition(function(r) {
+    //     if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+    //       //获取当前位置经纬度
+    //       var myGeo = new BMap.Geocoder();
+    //       myGeo.getLocation(new BMap.Point(r.point.lng, r.point.lat), function(
+    //         result
+    //       ) {
+    //         if (result) {
+    //           console.log(result);
+    //           self.latitude = result.point.lat;
+    //           self.longitude = result.point.lng;
+    //           self.drawMap();
+    //         }
+    //       });
+    //     }
+    //   });
+    // }
   },
   created() {
     settitle("地图");
@@ -128,7 +195,8 @@ export default {
 
   mounted() {
     console.log("我是登陆页面");
-    this.getAdd();
+    this.getSharedBonus(); //微信获取位置 百度绘制路线
+    // this.getAdd();
     // TMap("D2JBZ-JB4RQ-RVE5W-GVKCE-N6LBH-EWBFO").then(qq => {
     //   var map = new qq.maps.Map(document.getElementById("container"), {
     //     // 地图的中心地理坐标。
